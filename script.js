0983b19197d216aa6d6fef88ffa4483787ebac6b
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-05-21 12:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) {
+        return -Infinity;
+    }
+
+    let highest = -Infinity;
+    let secondHighest = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > highest) {
+            secondHighest = highest;
+            highest = arr[i];
+        } else if (arr[i] > secondHighest && arr[i] < highest) {
+            secondHighest = arr[i];
+        }
+    }
+
+    // Handle the case where all elements are the same
+    return secondHighest === -Infinity ? -Infinity : secondHighest;
+}
